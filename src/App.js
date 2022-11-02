d24279b27867f7575f1b9f5922fad36de55412ba
ShinChaeYoon <scy1011503@naver.com>
2022-11-02 19:28:47 +0900

    프론트 푸쉬

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./index.css"
+import React from "react"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./pages/home"
+import Signup from "./pages/signup"
+
+const App = () => {
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/*" element={<Home/>}/>
+        <Route path="/signup" element={<Signup/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
